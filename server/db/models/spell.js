85387db4f0cc2441c42af94fbb260a72ad768a97
November Williams <november@Novembers-MacBook-Air.local>
2018-07-23 16:55:17 -0400

    updated spell definition per request

--- server/db/models/spell.js
+++ server/db/models/spell.js
@@ -2,22 +2,25 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Spell = db.define('spell', {
-  name: {
+  title: {
     type: Sequelize.STRING,
     allowNull: false,
-    validate: { notEmpty: true },
+    validate: {notEmpty: true}
   },
-  imageUrl: {
-    type: Sequelize.STRING,
-    defaultValue:
-      'https://img4.wikia.nocookie.net/__cb20120706201912/finalfantasy/images/7/77/Black_Mage_Shibuya.png',
+  images: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    defaultValue: [
+      'https://img4.wikia.nocookie.net/__cb20120706201912/finalfantasy/images/7/77/Black_Mage_Shibuya.png'
+    ]
   },
-  stock: { type: Sequelize.INTEGER, allowNull: false },
+  quantity: {type: Sequelize.INTEGER, allowNull: false, defaultValue: 0},
   rating: {
     type: Sequelize.FLOAT,
     allowNull: false,
-    validate: { min: 0.0, max: 5.0 },
+    validate: {min: 0.0, max: 5.0}
   },
+  description: {type: Sequelize.TEXT},
+  price: {type: Sequelize.FLOAT}
 })
 
 module.exports = Spell
