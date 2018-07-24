b6495215600049e6749e65f8c9c82cb3549deb8e
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-07-24 15:42:05 -0400

    seed file in progress, spell model updated

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,17 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false
-  }
-)
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false
+//   }
+// )
+const db = new Sequelize('spellbinder', 'fsa', 'secretPassword123', {
+  logging: false,
+  dialect: 'postgres'
+})
+
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
--- server/db/models/spell.js
+++ server/db/models/spell.js
@@ -7,20 +7,35 @@ const Spell = db.define('spell', {
     allowNull: false,
     validate: {notEmpty: true}
   },
+
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+
   images: {
     type: Sequelize.ARRAY(Sequelize.STRING),
     defaultValue: [
       'https://img4.wikia.nocookie.net/__cb20120706201912/finalfantasy/images/7/77/Black_Mage_Shibuya.png'
     ]
   },
-  quantity: {type: Sequelize.INTEGER, allowNull: false, defaultValue: 0},
+
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+
   rating: {
     type: Sequelize.FLOAT,
-    allowNull: false,
-    validate: {min: 0.0, max: 5.0}
+    validate: {
+      min: 0.0,
+      max: 5.0
+    }
   },
-  description: {type: Sequelize.TEXT},
-  price: {type: Sequelize.FLOAT}
+  price: {type: Sequelize.STRING},
+  magic_school: {type: Sequelize.STRING} /* eslint-disable camelcase*/,
+  magickaCost: {type: Sequelize.STRING}
 })
 
 module.exports = Spell
