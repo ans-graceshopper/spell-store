4c73c8ee146bbe0247092b7391c0ddfbf153aa4a
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-07-25 00:15:33 -0400

    - fixed db so that it should log in without needing to switch. - fixed seed file. left fakeData seed commented out in case you have problems. - made small change to skyrim_spells.csv ('title' was previously called 'name'.

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,32 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+const createDatabase = () => {
+  if (process.env.USER === 'notnull') {
+    const db = new Sequelize('spellbinder', 'fsa', 'secretPassword123', {
+      dialect: 'postgres',
+    })
+    return db
+  } else {
+    const db = new Sequelize(
+      process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+      {
+        logging: false,
+      }
+    )
+    return db
+  }
+}
+
+const db = createDatabase()
+// Amy & November v
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false,
+//   }
+// )
+
 // Amy & November v
 // const db = new Sequelize(
 //   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
@@ -12,10 +38,7 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 // )
 
 // Sarah v
-const db = new Sequelize('spellbinder', 'fsa', 'secretPassword123', {
-  logging: false,
-  dialect: 'postgres',
-})
+//const db = new Sequelize()
 
 module.exports = db
 
