8e07bc8b78e8d6119e06f0192a357bf4c56a155a
Amy De Genaro <32823479+amydegenaro@users.noreply.github.com>
2018-07-31 16:05:00 -0400

    Revert "Seed for orders"

---
+++ server/api/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+module.exports = router
+
+router.use('/users', require('./users'))
+router.use('/spells', require('./spells'))
+router.use('/cart', require('./cart'))
+router.use('/orders', require('./orders'))
+router.use('/stripe', require('./stripe'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
