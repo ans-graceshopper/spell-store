1f7727e76164ae774de73bed757c76c5fa821c89
Amy De Genaro <amy.degenaro@gmail.com>
2018-07-31 11:06:29 -0400

    fixed order detail to not render until spells[0] is truthy

--- client/components/user-order-detail.js
+++ client/components/user-order-detail.js
@@ -9,7 +9,7 @@ class OrderDetail extends Component {
     //this.props.fetchCurrentOrder(this.props.match.params.id)
   }
   render() {
-    if (!this.props.spells) return <div>no spells.</div>
+    if (!this.props.spells[0]) return <div>no spells.</div>
     return (
       <div>
         <h3>Order Details</h3>
