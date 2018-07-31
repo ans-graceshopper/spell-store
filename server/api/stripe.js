29cc48443d9b2d036751e669ff07d907c7dce389
Amy De Genaro <amy.degenaro@gmail.com>
2018-07-31 15:14:21 -0400

    fixed stripe checkout to actually update inventory only when successful payment

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -3,18 +3,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET)
 const {Order, Spell} = require('../db/models')
 module.exports = router
 
-const postStripeCharge = res => (stripeErr, stripeRes) => {
+const postStripeCharge = (res, req) => async (stripeErr, stripeRes) => {
   if (stripeErr) {
     res.status(500).send({error: stripeErr})
   } else {
     res.status(200).send({success: stripeRes})
-  }
-}
-
-router.post('/', async (req, res, next) => {
-  try {
-    console.log('req.body.metadata .............', req.body.metadata)
-    stripe.charges.create(req.body, postStripeCharge(res))
     const formerCart = await Order.findById(req.body.metadata.id)
     await formerCart.update({
       isCart: false,
@@ -22,12 +15,19 @@ router.post('/', async (req, res, next) => {
       total: req.body.amount,
     })
     // deduct inventory with metadata.spells
-//     req.body.metadata.spells.forEach(async spell => {
-//       const foundSpell = await Spell.findById(spell.id)
-//       foundSpell.update({inventory: foundSpell.inventory - spell.quantity})
-//     })
+    const spells = await formerCart.getSpells()
+    spells.forEach(async spell => {
+      await spell.update({
+        inventory: spell.inventory - spell.spellorders.quantity,
+      })
+    })
+  }
+}
 
-    // await Promise.all(updateSpells)
+router.post('/', (req, res, next) => {
+  try {
+    console.log('METADATA::::::::::::::', req.body.metadata)
+    stripe.charges.create(req.body, postStripeCharge(res, req))
   } catch (e) {
     next(e)
   }
